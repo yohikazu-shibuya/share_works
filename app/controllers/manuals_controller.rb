8a68843112283b697136df00a8521409667959cf
yohikazu-shibuya <ys0979@gmail.com>
2021-08-10 22:33:36 +0900

    view_controller修正

--- app/controllers/manuals_controller.rb
+++ app/controllers/manuals_controller.rb
@@ -1,2 +1,23 @@
 class ManualsController < ApplicationController
+  def index
+    @manuals = Manual.all
+  end
+
+  def new
+    @manual = Manual.new
+  end
+
+  def create
+    @manual = Manual.create(manual_params)
+    if @manual.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def manual_params
+    params.require(:type_qa, :manual, :subject, :question, :answer).permit(:notice)
+  end
 end
